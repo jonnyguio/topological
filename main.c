b9481a2ad48d91ef36ee77ffd127b306cf17a336
Jonny-UFRJ <joaoluisguio@gmail.com>
2015-04-29 15:02:04 -0300

    FINALLY

--- main.c
+++ main.c
@@ -22,29 +22,30 @@ int main() {
   name = malloc(sizeof(char) * (m + (m - 1) + 1)); // + 1 because of null
   fgets(name, 9, stdin); // forcing to jump one line
   for (i = 0; i < n; i++) {
-    //scanf("%d", &number);
+    name[0] = '\n';
     fgets(name, 9, stdin);
-      if (name[0] != '\n') {
-        strtok(name, "\n");
-        s = name;
-        for (numbers = 0; s[numbers] != '\0'; s[numbers] == ' '? numbers++ : *s++);
-        numbers++;
-        g[i].size = numbers;
-        char *endPtr;
-        endPtr = name;
-        for (j = numbers; j > 0; j--) {
-          g[i].adj[(numbers-j)] = (int) strtol(endPtr, &endPtr, 10);
-          g[g[i].adj[(numbers-j)]-1].grau++;
-        }
+    if (name[0] != '\n') {
+      strtok(name, "\n");
+      s = name;
+      for (numbers = 0; s[numbers] != '\0'; s[numbers] == ' '? numbers++ : *s++);
+      numbers++;
+      g[i].size = numbers;
+      char *endPtr;
+      endPtr = name;
+      for (j = numbers; j > 0; j--) {
+        g[i].adj[(numbers-j)] = (int) strtol(endPtr, &endPtr, 10);
+        g[g[i].adj[(numbers-j)]-1].grau++;
       }
-      else {
-        name[0] = ' ';
-      }
+    }
+    else {
+      name[0] = ' ';
+      g[i].size = 0;
+    }
   }
 
   int *q, *result;
   q = malloc(sizeof(int)*n);
-  //result = malloc(sizeof(int)*n);
+
   counter = 0;
   for (i = 0; i < n; i++) {
     q[i] = 0;
@@ -53,20 +54,21 @@ int main() {
       counter++;
     }
   }
-  //puts("");
+
   j = 0;
   while (q[n-1] != -1) {
+    
     printf("%d ", q[j]);
-    q[j] = -1;
 
-    for (i = 0; i < g[j].size; i++) {
-      //printf("%d - %d - %d\n", j, i, g[j].adj[i]-1);
-      g[g[j].adj[i] - 1].grau--;
-      if (g[g[j].adj[i] - 1].grau == 0) {
-        q[counter] = g[g[j].adj[i] - 1].i;
+    for (i = 0; i < g[q[j]-1].size; i++) {
+      if (--g[g[q[j]-1].adj[i] - 1].grau == 0) {
+        q[counter] = g[g[q[j]-1].adj[i] - 1].i;
         counter++;
       }
     }
+
+    q[j] = -1;
     j++;
   }
+  puts("");
 }
