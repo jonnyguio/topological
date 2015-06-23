0bb9f907d63ec5aff7f3a1c40fd1ec7e4ef4dbbe
= <=>
2015-06-23 00:10:06 -0300

    Finally, it's done

--- main.c
+++ main.c
@@ -12,31 +12,33 @@ typedef struct node {
   struct adj *adjInit;
 } node;
 
-void insertNode(node *init, int index, int adj) {
-    node *aux;
-    if (init == (node *) NULL) {
-        init = (node *) malloc(sizeof(node));
-        init->next = NULL;
-        init->i = index;
-        init->adjInit = NULL;
-        init->grau = 0;
+void insertNode(node **init, int index, int adj) {
+    node **aux;
+    if (*init == NULL) {
+        *init = (node *) malloc(sizeof(node));
+        (*init)->next = NULL;
+        (*init)->i = index;
+        (*init)->adjInit = NULL;
+        (*init)->grau = 0;
+        (*init)->size = 0;
     }
     else {
-        for (aux = init; aux->next; aux = aux->next);
-        aux->next = (node *) malloc(sizeof(node));
-        aux->next->next = (node *) NULL;
-        aux->next->i = index;
-        aux->next->adjInit = NULL;
-        aux->next->grau = 0;
+        for (aux = init; (*aux)->next; aux = &((*aux)->next));
+        (*aux)->next = (node *) malloc(sizeof(node));
+        (*aux)->next->next = (node *) NULL;
+        (*aux)->next->i = index;
+        (*aux)->next->adjInit = NULL;
+        (*aux)->next->grau = 0;
+        (*aux)->next->size = 0;
     }
 
 }
 
-void setSize(node *init, int index, int size) {
-    node *aux;
-    for (aux = init; aux; aux = aux->next) {
-        if (aux->i == index) {
-            aux->size = size;
+void setSize(node **init, int index, int size) {
+    node **aux;
+    for (aux = init; *aux; aux = &((*aux)->next)) {
+        if ((*aux)->i == index) {
+            (*aux)->size = size;
             break;
         }
     }
@@ -51,14 +53,21 @@ int getSize(node *init, int index) {
     return -1;
 }
 
-void setAdj(adj *init, int index, int val) {
-    adj *aux;
-    for (aux = init; aux; aux = aux->next) {
-        if (aux->i = index) {
-            aux->i = val;
+void setAdj(adj **init, int index, int val) {
+    adj **aux;
+    int i = 1;
+    aux = init;
+    while (i < index) {
+        aux = &((*aux)->next);
+        i++;
+    }
+    (*aux)->i = val;
+    /*for (aux = init; *aux; aux = &((*aux)->next)) {
+        if ((*aux)->i = index) {
+            (*aux)->i = val;
             break;
         }
-    }
+    }*/
 }
 
 int getAdj(adj *init, int iterations) {
@@ -71,30 +80,18 @@ int getAdj(adj *init, int iterations) {
     return aux->i;
 }
 
-int getOnAdj(node *init, int index, int indexAdj) {
-    node *aux;
-    adj *aux2;
-    for (aux = init; aux; aux = aux->next) {
-        for (aux2 = aux->adjInit; aux2; aux2 = aux2->next) {
-                return aux2->i;
-            }
-        }
-    }
-
-}
-
-void insertOnAdj(adj *init, int val) {
-    adj *aux;
-    if (init != (adj *) NULL) {
-        for (aux = init; aux->next; aux = aux->next);
-        aux->next = (adj *) malloc(sizeof(adj));
-        aux->next->next = (adj *) NULL;
-        aux->next->i = val;
+void insertOnAdj(adj **init, int val) {
+    adj **aux;
+    if (*init != (adj *) NULL) {
+        for (aux = init; (*aux)->next != NULL; aux = &((*aux)->next));
+        (*aux)->next = (adj *) malloc(sizeof(adj));
+        (*aux)->next->next = (adj *) NULL;
+        (*aux)->next->i = val;
     }
     else {
-        init = (adj *) malloc(sizeof(adj));
-        init->next = (adj *) NULL;
-        init->i = val;
+        *init = (adj *) malloc(sizeof(adj));
+        (*init)->next = (adj *) NULL;
+        (*init)->i = val;
     }
 }
 
@@ -102,17 +99,26 @@ void insertAdj(node *init, int index, int iadj) {
     node *aux;
     for (aux = init; aux; aux = aux->next) {
         if (aux->i == index) {
-            insertOnAdj(aux->adj, iadj);
+            insertOnAdj(&(aux->adjInit), iadj);
             break;
         }
     }
 }
 
-void increaseGrau(node *init, int index) {
-    node *aux;
-    for (aux = init; aux; aux = aux->next) {
-        if (aux->i == index) {
-            aux->grau++;
+void increaseGrau(node **init, int index) {
+    node **aux;
+    for (aux = init; *aux; aux = &((*aux)->next)) {
+        if ((*aux)->i == index) {
+            (*aux)->grau++;
+        }
+    }
+}
+
+void decreaseGrau(node **init, int index) {
+    node **aux;
+    for (aux = init; *aux; aux = &((*aux)->next)) {
+        if ((*aux)->i == index) {
+            (*aux)->grau--;
         }
     }
 }
@@ -127,83 +133,123 @@ int getGrau(node *init, int index) {
     return -1;
 }
 
+void printAdj(adj *init) {
+    adj *aux;
+    for (aux = init; aux; aux = aux->next) {
+        printf("%d |", aux->i);
+    }
+    printf("\n");
+}
+
+void printNode(node *init) {
+    node *aux;
+    for (aux = init; aux; aux = aux->next) {
+        printf("%d - %d - %d\n", aux->i, aux->grau, aux->size);
+        printAdj(aux->adjInit);
+    }
+    printf("\n");
+}
+
+int getOnAdj(node *init, int index, int indexAdj) {
+    node *aux;
+    adj *aux2;
+    int i;
+    for (aux = init; aux; aux = aux->next) {
+        if (aux->i == index)
+            break;
+    }
+    if (aux) {
+        aux2 = aux->adjInit;
+        i = 1;
+        while (i < indexAdj) {
+            aux2 = aux2->next;
+            i++;
+        }
+    }
+    return aux2->i;
+}
+
 int main() {
-  int i, j, n, m, counter, numbers, add;
-  node *init;
-  char *name, *s;
+    int i, j, n, m, counter, numbers, add;
+    node *init;
+    char *name, *s;
 
-  scanf("%d", &n); scanf("%d", &m);
-  init = (node *) malloc(sizeof(node)*n);
-  for (i = 0; i < n; i++) {
-    insertNode(init, i + 1, m)
-  }
-
-  counter = 0;
-  name = malloc(sizeof(char) * (m + (m - 1) + 1)); // + 1 because of null
-  fgets(name, 9, stdin); // forcing to jump one line
-
-  for (i = 0; i < n; i++) {
-
-    name[0] = '\n';
-    fgets(name, 9, stdin);
-
-    if (name[0] != '\n') {
-      strtok(name, "\n");
-
-      s = name;
-      for (numbers = 0; s[numbers] != '\0'; s[numbers] == ' '? numbers++ : *s++);
-      numbers++;
-      setSize(init, i + 1, numbers);
-      //g[i].size = numbers;
-
-      char *endPtr;
-      endPtr = name;
-      for (j = numbers; j > 0; j--) {
-        add = (int) strtol(endPtr, &endPtr, 10);
-        insertAdj(init, numbers - j + 1, add);
-        //g[i].adj[(numbers-j)] = (int) strtol(endPtr, &endPtr, 10);
-        increaseGrau(init, add);
-        //g[g[i].adj[(numbers-j)]-1].grau++;
-      }
+    scanf("%d", &n); scanf("%d", &m);
+    init = (node *) NULL;
+    for (i = 0; i < n; i++) {
+    insertNode(&init, i + 1, m);
     }
-    else {
-      name[0] = ' ';
-      setSize(init, i + 1, 0);
-      //g[i].size = 0;
+
+
+    counter = 0;
+    name = malloc(sizeof(char) * (m + (m - 1) + 1)); // + 1 because of null
+    fgets(name, 9, stdin); // forcing to jump one line
+
+    for (i = 0; i < n; i++) {
+
+        name[0] = '\n';
+        fgets(name, 9, stdin);
+        if (name[0] != '\n') {
+          strtok(name, "\n");
+
+          s = name;
+          for (numbers = 0; s[numbers] != '\0'; s[numbers] == ' '? numbers++ : *s++);
+          numbers++;
+          setSize(&init, i + 1, numbers);
+          //g[i].size = numbers;
+
+          char *endPtr;
+          endPtr = name;
+          for (j = numbers; j > 0; j--) {
+            add = (int) strtol(endPtr, &endPtr, 10);
+            insertAdj(init, i + 1, add);
+            //g[i].adj[(numbers-j)] = (int) strtol(endPtr, &endPtr, 10);
+            increaseGrau(&init, add);
+            //g[g[i].adj[(numbers-j)]-1].grau++;
+          }
+        }
+        else {
+          name[0] = ' ';
+          setSize(&init, i + 1, 0);
+          //g[i].size = 0;
+        }
     }
-  }
 
-  adj *q;
-  //int *q;
-  q = (adj *) malloc(sizeof(adj));
-  //q = malloc(sizeof(int)*n);
+    adj *q;
+    //int *q;
+    q = NULL;
+    //q = malloc(sizeof(int)*n);
 
-  counter = 0;
-  for (i = 0; i < n; i++) {
-    //q[i] = 0;
-    insertOnAdj(q, 0);
-    //if (g[i].grau == 0) {
-    if (getSize(init, i + 1)) {
-      setAdj(q, counter + 1, i + 1);
-      //q[counter] = g[i].i;
-      counter++;
+    counter = 0;
+    for (i = 0; i < n; i++) {
+        //q[i] = 0;
+        insertOnAdj(&q, 0);
+        //if (g[i].grau == 0) {
+        if (getGrau(init, i + 1) == 0) {
+            setAdj(&q, counter + 1, i + 1);
+            //q[counter] = g[i].i;
+            counter++;
+        }
     }
-  }
 
-  j = 0;
-  while (getAdj(q, n - 1) != -1) { // q[n-1]
+    j = 0;
+    while (getAdj(q, n - 1) != -1) { // q[n-1]
 
-    printf("%d ", getAdj(q, j)); // q[j]
+        //printNode(init);
 
-    for (i = 0; i < getSize(init, getAdj(q, j)); i++) { // g[q[j]-1].size
-      if (getGrau(init, getOnAdj(init, getAdj(q, j), i)) - 1 == 0) { // --g[g[q[j]-1].adj[i] - 1].grau
-        setAdj(q, counter + 1, getOnAdj(init, getAdj(q, j), i)); //q[counter] = g[g[q[j]-1].adj[i] - 1].i;
-        counter++;
-      }
+        printf("%d\n", getAdj(q, j)); // q[j];
+
+        for (i = 0; i < getSize(init, getAdj(q, j)); i++) { // g[q[j]-1].size
+            decreaseGrau(&init, getOnAdj(init, getAdj(q, j), i + 1));
+            if (getGrau(init, getOnAdj(init, getAdj(q, j), i + 1)) == 0) { // --g[g[q[j]-1].adj[i] - 1].grau
+                setAdj(&q, counter + 1, getOnAdj(init, getAdj(q, j), i + 1)); //q[counter] = g[g[q[j]-1].adj[i] - 1].i;
+                counter++;
+            }
+        }
+
+        setAdj(&q, j + 1, -1);
+        //q[j] = -1;
+        j++;
     }
-    setAdj(q, j, -1);
-    //q[j] = -1;
-    j++;
-  }
-  puts("");
+    puts("");
 }
